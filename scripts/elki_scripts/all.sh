a329913aaa653c6605e40f4577e8e2778e9cb6e6
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-04-04 18:56:36 +0200

    Fix the all.sh script

--- scripts/elki_scripts/all.sh
+++ scripts/elki_scripts/all.sh
@@ -9,24 +9,33 @@ else
     dist="minkowski.EuclideanDistanceFunction"
 fi
 
-strategyIndices=( nobulk str astr onedim binsplit hilbert peano zcurve)
+strategyIndices=( str astr onedim binsplit hilbert peano zcurve nobulk)
 
-strategies[nobulk]=" "
-strategies[str]="-spatial.bulkstrategy SortTileRecursiveBulkSplit"
-strategies[astr]="-spatial.bulkstrategy AdaptiveSortTileRecursiveBulkSplit"
-strategies[onedim]="-spatial.bulkstrategy OneDimSortBulkSplit"
-strategies[binsplit]="-spatial.bulkstrategy SpatialSortBulkSplit -rtree.bulk.spatial-sort BinarySplitSpatialSorter"
-strategies[hilbert]="-spatial.bulkstrategy SpatialSortBulkSplit -rtree.bulk.spatial-sort HilbertSpatialSorter"
-strategies[peano]="-spatial.bulkstrategy SpatialSortBulkSplit -rtree.bulk.spatial-sort PeanoSpatialSorter"
-strategies[zcurve]="-spatial.bulkstrategy SpatialSortBulkSplit -rtree.bulk.spatial-sort ZCurveSpatialSorter"
+strategies[0]="-spatial.bulkstrategy SortTileRecursiveBulkSplit"
+strategies[1]="-spatial.bulkstrategy AdaptiveSortTileRecursiveBulkSplit"
+strategies[2]="-spatial.bulkstrategy OneDimSortBulkSplit"
+strategies[3]="-spatial.bulkstrategy SpatialSortBulkSplit -rtree.bulk.spatial-sort BinarySplitSpatialSorter"
+strategies[4]="-spatial.bulkstrategy SpatialSortBulkSplit -rtree.bulk.spatial-sort HilbertSpatialSorter"
+strategies[5]="-spatial.bulkstrategy SpatialSortBulkSplit -rtree.bulk.spatial-sort PeanoSpatialSorter"
+strategies[6]="-spatial.bulkstrategy SpatialSortBulkSplit -rtree.bulk.spatial-sort ZCurveSpatialSorter"
+strategies[7]=" "
 
 mkdir -p $4
 
-for strategy in "${strategyIndices[@]}"
+# for strategy in "${strategyIndices[@]}"
+# do
+#     echo "Start $strategy"
+#     echo "./elkinogui.sh -dbc.in $1 -db.index tree.spatial.rstarvariants.rstar.RStarTreeFactory -pagefile.pagesize 4096 ${strategies[$strategy]} -time -algorithm benchmark.RangeQueryBenchmarkAlgorithm -algorithm.distancefunction $dist -rangebench.query FileBasedDatabaseConnection -dbc.in $2 -evaluator NoAutomaticEvaluation -resulthandler ResultWriter | grep --invert-match '#'"
+#     time ./elkinogui.sh -dbc.in $1 -db.index tree.spatial.rstarvariants.rstar.RStarTreeFactory -pagefile.pagesize 4096 ${strategies[$strategy]} -time -algorithm benchmark.RangeQueryBenchmarkAlgorithm -algorithm.distancefunction $dist -rangebench.query FileBasedDatabaseConnection -dbc.in $2 -evaluator NoAutomaticEvaluation -resulthandler ResultWriter | grep --invert-match '#' > $4/$strategy.txt
+#     echo "Finshed $strategy"
+#     echo ""
+# done
+
+for ((i = 0; i < ${#strategies[@]}; i++))
 do
-    echo "Start $strategy"
-    echo "./elkinogui.sh -dbc.in $1 -db.index tree.spatial.rstarvariants.rstar.RStarTreeFactory -pagefile.pagesize 4096 ${strategies[$strategy]} -time -algorithm benchmark.RangeQueryBenchmarkAlgorithm -algorithm.distancefunction $dist -rangebench.query FileBasedDatabaseConnection -dbc.in $2 -evaluator NoAutomaticEvaluation -resulthandler ResultWriter | grep --invert-match '#'"
-    time ./elkinogui.sh -dbc.in $1 -db.index tree.spatial.rstarvariants.rstar.RStarTreeFactory -pagefile.pagesize 4096 ${strategies[$strategy]} -time -algorithm benchmark.RangeQueryBenchmarkAlgorithm -algorithm.distancefunction $dist -rangebench.query FileBasedDatabaseConnection -dbc.in $2 -evaluator NoAutomaticEvaluation -resulthandler ResultWriter | grep --invert-match '#' > $4/$strategy.txt
-    echo "Finshed $strategy"
+    echo "Start ${strategyIndices[$i]}"
+    echo "./elkinogui.sh -dbc.in $1 -db.index tree.spatial.rstarvariants.rstar.RStarTreeFactory -pagefile.pagesize 4096 ${strategies[$i]} -time -algorithm benchmark.RangeQueryBenchmarkAlgorithm -algorithm.distancefunction $dist -rangebench.query FileBasedDatabaseConnection -dbc.in $2 -evaluator NoAutomaticEvaluation -resulthandler ResultWriter | grep --invert-match '#'"
+    time ./elkinogui.sh -dbc.in $1 -db.index tree.spatial.rstarvariants.rstar.RStarTreeFactory -pagefile.pagesize 4096 ${strategies[$i]} -time -algorithm benchmark.RangeQueryBenchmarkAlgorithm -algorithm.distancefunction $dist -rangebench.query FileBasedDatabaseConnection -dbc.in $2 -evaluator NoAutomaticEvaluation -resulthandler ResultWriter | grep --invert-match '#' > $4/${strategyIndices[$i]}.txt
+    echo "Finshed ${strategyIndices[$i]}"
     echo ""
 done
